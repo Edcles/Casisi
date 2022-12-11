9d26abf802de9c039a1dd483f56efca4605c5a30
pepiillo99 <pepiillo99@hotmail.com>
2022-12-11 23:28:32 +0100

    Add files via upload

--- src/we/josemipepeedu/casisi/Casisi.java
+++ src/we/josemipepeedu/casisi/Casisi.java
@@ -1,7 +1,39 @@
 package we.josemipepeedu.casisi;
 
-public class Casisi {
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.util.HashMap;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import we.josemipepeedu.casisi.Tragaperras.Tragaperras;
+
+
+public class Casisi extends JFrame {
+	private HashMap<String, JPanel> screens = new HashMap<String, JPanel>();
 	public static void main(String[] args) {
-		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Casisi frame = new Casisi();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Casisi() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getClassLoader().getResource("logo.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1200, 800);
+		screens.put("game-tragaperras", new Tragaperras());
+		setContentPane(screens.get("game-tragaperras"));
+	}
+
 }
---
+++ src/we/josemipepeedu/casisi/Tragaperras/Spin.java
@@ -0,0 +1,94 @@
+package we.josemipepeedu.casisi.Tragaperras;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+import we.josemipepeedu.casisi.Utils.BackgroundType;
+import we.josemipepeedu.casisi.Utils.ImagePanel;
+import we.josemipepeedu.casisi.Utils.Utils;
+
+public class Spin extends Thread {
+	private Tragaperras tragaperras;
+	private int x = 0;
+	private List<ImagePanel> items = new ArrayList<ImagePanel>();
+	private int speed = 0;
+	private int ySpin = 600;
+	private boolean toStop = false;
+	private long lastSpeedChanged = 0;
+	private int changeSpeedDifference = 200;
+	public Spin(Tragaperras tragaperras, int x) {
+		this.tragaperras = tragaperras;
+		this.x = x;
+	}
+	public boolean isSpinning() {
+		return speed != 0;
+	}
+	public boolean isToStop() {
+		return toStop;
+	}
+	public void toStart() {
+		speed = 10;
+		toStop = false;
+		if (!isAlive()) {
+			start();
+		}
+	}
+	public void toStop() {
+		toStop = true;
+		lastSpeedChanged = System.currentTimeMillis();
+	}
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if (speed != 0) {
+				boolean needCreateOther = items.isEmpty();
+				int lastY = 0;
+				Iterator<ImagePanel> iterator = items.iterator();
+				while (iterator.hasNext()) {
+					JPanel panel = iterator.next();
+					panel.setBounds(panel.getBounds().x, panel.getBounds().y - speed, panel.getBounds().width, panel.getBounds().height);
+					panel.repaint();
+					if (panel.getBounds().getY() < ySpin - 300) {
+						panel.setVisible(false);
+						iterator.remove();
+					}
+					if (!iterator.hasNext()) {
+						needCreateOther = panel.getBounds().getY() < ySpin - 50;
+						if (needCreateOther) {
+							lastY = (int) panel.getBounds().getY();
+						}
+					}
+				}
+				if (needCreateOther) {
+					int randomID = Utils.random.nextInt(14) + 1;
+					try {
+						ImagePanel imagePanel = new ImagePanel(ImageIO.read(getClass().getClassLoader().getResource("tragaperras/" + randomID + ".png")));
+						//imagePanel.setBackground(new Color(Utils.random.nextInt(255), Utils.random.nextInt(255), Utils.random.nextInt(255)));
+						imagePanel.setBackgroundType(BackgroundType.PANEL);
+						imagePanel.setBounds(x, lastY + 50, 50, 50);
+						items.add(imagePanel);
+						tragaperras.add(imagePanel);
+					} catch (Exception e) {
+						System.out.println(randomID + " dont exists?");
+						e.printStackTrace();
+					}
+				}
+				if (toStop && (lastSpeedChanged + changeSpeedDifference) - System.currentTimeMillis() <= 0) {
+					speed--;
+					lastSpeedChanged = System.currentTimeMillis();
+				}
+			}
+		}
+	}
+}
