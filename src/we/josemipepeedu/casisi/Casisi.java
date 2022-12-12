f8947f1e5a8726880d24f4fc5e0a7613346f0e38
Edcles <eduardoclarosescalante@gmail.com>
2022-12-12 15:47:44 +0100

    Cambios 12/12

--- src/we/josemipepeedu/casisi/Casisi.java
+++ src/we/josemipepeedu/casisi/Casisi.java
@@ -8,8 +8,10 @@ public class Casisi extends JFrame {
 	
 	public Casisi() {
 		setTitle("Casisi");
-		setMinimumSize(new Dimension(1200, 800));
+		setSize(new Dimension(1200, 800));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setContentPane(new Ruleta(this));
+		setResizable(false);
 		setVisible(true);
 	}
 	
